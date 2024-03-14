5dd595b78508333e025a657cdc5c5b51f84e12f7
Rasha9393 <rashadarwish93@gmail.com>
2024-03-14 08:58:28 +0100

    created project

--- g49-git-lecture/src/main/java/se/lexicon/App.java
+++ g49-git-lecture/src/main/java/se/lexicon/App.java
@@ -1,14 +1,44 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
+    /*public static void main( String[] args ) {
+        Scanner scanner=new Scanner(System.in) ;
+        System .out.println("enter name and last name");
+        String contact= scanner.next();
+        String fondContact = ContectData.findContact(contact);
+        if (fondContact==null){
+            System.out.println("not found");
+        }else{
+            System.out.println(contact);
+        }
 
-        System.out.println( "Hello World!" );
+
+    }*/
+
+    public static void main( String[] args ) {
+        Scanner scanner=new Scanner(System.in) ;
+        System.out.println("number ");
+
+        String[] contactStorage= new String[Scanner.next()];
+        for (int i=0;i< contactStorage.length;i++)
+
+        System.out.println("First name  lastName");
+
+
+        while (true) {
+            System.out.println("enter lastname");
+            String contact = scanner.next();
+            String fondContact = ContectData.findContactBynumber(contact);
+            if (fondContact == null) {
+                System.out.println("not found");
+            } else {
+                System.out.println(fondContact);
+            }
+        }
+
     }
+
 }
